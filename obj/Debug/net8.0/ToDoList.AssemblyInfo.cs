50486ca908cb6c67ed79c78f305e4f7575f98316
KateGordin <kate.gordin@gmail.com>
2024-04-30 14:59:05 +0200

    Update assembly version in ToDoList.AssemblyInfo.cs Update cache hash in ToDoList.AssemblyInfoInputs.cache Add ToDoList.sourcelink.json to project file list Add ToDoList.sourcelink.json file

--- obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d88c2910539b1023e2248efa46acbcf5a6b2e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
