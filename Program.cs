e08289ba4a1be5dd04b5ed94776d1e69b9ab0d87
KateGordin <kate.gordin@gmail.com>
2024-04-30 15:57:17 +0200

    Add user input options and variables in Program.cs Update assembly version in ToDoList.AssemblyInfo.cs Update cache hash in ToDoList.AssemblyInfoInputs.cache Update sourcelink URL in ToDoList.sourcelink.json

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
+Console.WriteLine(userInput);
+
+userInput = "ABC";
+Console.WriteLine(userInput);
+
+int number;
+number = 8;
+Console.WriteLine(number);
+
+int a = 1, b = 6;
+string name, lastName;
+
 Console.ReadKey();
