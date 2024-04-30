c945b1d0e0c653efe729e4471cdc6cfa30bd4928
KateGordin <kate.gordin@gmail.com>
2024-04-30 17:05:38 +0200

    Update user interface and input handling in Program.cs Update assembly version and cache hash in ToDoList.AssemblyInfo.cs and ToDoList.AssemblyInfoInputs.cache Update sourcelink URL in ToDoList.sourcelink.json

--- Program.cs
+++ Program.cs
@@ -1,16 +1,11 @@
-﻿int a = 10;
-int b = 5;
-++a;
---b;
-Console.WriteLine(a);
-Console.WriteLine(b);
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
 
-Console.WriteLine("Addition:" + (a + b));
-Console.WriteLine("Subtraction:" + (a - b));
-Console.WriteLine("Multiplication:" + a * b);
-Console.WriteLine("Division:" + a / b);
-Console.WriteLine("John" + " " + "Smith");
-string word = "abc";
-Console.WriteLine(word + a);
+string userChoice = Console.ReadLine();
+Console.WriteLine("User input: " + userChoice);
 
 Console.ReadKey();
--- obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e08289ba4a1be5dd04b5ed94776d1e69b9ab0d87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84ab5973b481cd5af0cede050700f1346353cdc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
